6d42b4ade2a92b240629406ed83f986c239cc4fa
AldieNightStar <AldieNightStar@gmail.com>
2024-09-26 22:55:30 +0200

    Simple Love.run

--- src/main/kotlin/haxidenti/firefly/love/Love.kt
+++ src/main/kotlin/haxidenti/firefly/love/Love.kt
@@ -5,10 +5,6 @@ import java.io.File
 
 object Love {
     fun run(folder: File, output: (String) -> Unit) {
-        if (OS.isWindows) {
-            OS.runCmd("lovec", arrayOf("."), folder, output)
-        } else if (OS.isUnix) {
-            OS.runCmd("love", arrayOf("."), folder, output)
-        }
+        OS.runCmd("love", arrayOf("."), folder, output)
     }
 }
